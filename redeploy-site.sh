aba219c201fe4459d8ae684f5d38f8faca7e4b11
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-07-15 05:01:51 -0700

    optimize

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,12 +3,13 @@
 echo "Flask server starting"
 
 tmux kill-server
+cd $HOME/projects/mlh_port
+git fetch && git reset origin/main --hard
 tmux new -d -s flask_temp
-tmux send-keys -t flask "cd $HOME/projects/mlh_port" ENTER
-tmux send-keys -t flask "git fetch && git reset origin/main --hard" ENTER
 tmux send-keys -t flask "source venv/bin/activate.fish && pip install -r requirements.txt" ENTER
-tmux kill-server
+cd ./
 
+tmux kill-server
 systemctl restart myportfolio
 
 echo "Flask server successfully started"
