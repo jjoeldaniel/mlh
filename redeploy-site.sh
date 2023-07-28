e632199b5bbf64a3aa61fb1467d2605b5140b655
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-07-28 14:56:58 -0700

    fix git reset

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,7 +3,7 @@
 echo "Flask server starting"
 
 cd ~/mlh
-git fetch && reset origin/main --hard
+git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
 
